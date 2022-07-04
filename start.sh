4f5f25a0a8e7ae0f68dbda482c12d84fdfbdcf9f
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2022-07-05 00:39:47 +0300

    minor fix

---
+++ start.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+sudo apt update -y
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
